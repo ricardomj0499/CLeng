b182454b0ce5adaebedc32797b1b623817e28bff
ricardomj0499 <ricardomj0499@hotmail.com>
2020-12-13 11:37:23 -0600

    detecta flechas

--- main.c
+++ main.c
@@ -26,8 +26,8 @@ void mover(int cant, GtkWidget *layout,GtkWidget *window, GtkWidget *image2)
 	int x = 220;
 	int a;
 	for (a = 0; a < cant; a = a + 1){
-			image = gtk_image_new_from_file(buf1);
-			gtk_layout_put(GTK_LAYOUT(layout), image, x, y);
+			//image = gtk_image_new_from_file(buf1);
+			gtk_layout_put(GTK_LAYOUT(layout), image2, x, y);
             while(gtk_events_pending())
 	            gtk_main_iteration();
             cooldown(1);
@@ -37,37 +37,53 @@ void mover(int cant, GtkWidget *layout,GtkWidget *window, GtkWidget *image2)
 
 }
 
-void redraw(){
-
+gboolean my_keypress_function (GtkWidget *widget, GdkEventKey *event, gpointer data) {
+    if (event->keyval == GDK_KEY_space){
+        printf("SPACE KEY PRESSED!");
+        return TRUE;
+    }
+    return FALSE;
 }
 
 int main( int argc, char *argv[])
 {
     GtkWidget *window;
     GtkWidget *layout;
+    GtkWidget *canvas;
     GtkWidget *image;
     GtkWidget *image2;
-    GtkWidget *button;
-
+    
     char *pacman1 = realpath("pacman1.png", buf1);
 	char *maze = realpath("maze.jpg", buf);
 
     gtk_init(&argc, &argv);
 
+    //se inicia el window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size(GTK_WINDOW(window), 464, 600);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_title (GTK_WINDOW(window), "PacMan");
     gtk_window_set_resizable (GTK_WINDOW(window),FALSE);
 
+    //Se agregan eventos de teclado
+    gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
+    g_signal_connect (G_OBJECT(window), "key_press_event", G_CALLBACK(my_keypress_function), NULL);
+
+    //se agrega layout para la imagen
     layout = gtk_layout_new(NULL, NULL);
     gtk_container_add(GTK_CONTAINER (window), layout);
-    gtk_widget_show(layout);
+    //gtk_widget_show(layout);
 
+    //Se agrega la imagen
     image = gtk_image_new_from_file(buf);
     gtk_layout_put(GTK_LAYOUT(layout), image, 0, 0);
 
+    //se crea el area de dibujo
+   canvas = gtk_drawing_area_new();
+    gtk_container_add(GTK_CONTAINER(window), canvas);
+
     g_signal_connect_swapped(G_OBJECT(window), "destroy",G_CALLBACK(gtk_main_quit), NULL);
+
     gtk_widget_show_all(window);
     cooldown(2);
     image2 = gtk_image_new_from_file(buf1);
